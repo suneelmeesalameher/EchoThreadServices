d18de2d7e203f3d2f49699503b86bccaac2844bd
Meher Suneel <suneelmeesalameher@gmail.com>
2023-10-25 17:20:48 -0400

    Update users_routes.js

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -13,7 +13,18 @@ router.get("/", async (req, res) => {
   }
 });
 
-/*Create users */
+/* GET search users. */
+//give emailId here
+router.get("/:emailId", async (req, res) => {
+  try {
+    const Users = await User.find({ emailId: { $regex: req.params.emailId } });
+    res.json(Users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/*POST Create users */
 router.post("/", async (req, res) => {
   const users = await User({
     userId: uuid.v1(),
