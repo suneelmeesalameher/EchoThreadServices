42727cf5aed5e904a94354b519f5649f63ee6de5
Meher Suneel <suneelmeesalameher@gmail.com>
2023-10-27 16:36:00 -0400

    cors

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const port = 6000;
 var app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // You can specify the allowed origins here
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use("/user", usersRouter);
 
 mongoose.set("strictQuery", false);
