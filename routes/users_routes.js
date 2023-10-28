fa1afd166222a2a8bb497e09b8935c8b9f39ce25
Meher Suneel <suneelmeesalameher@gmail.com>
2023-10-28 16:24:53 -0400

    hashing+ salting

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -1,8 +1,10 @@
 var express = require("express");
+const crypto = require("crypto");
 var router = express.Router();
 const uuid = require("uuid");
 var User = require("../models/user");
 var Chat = require("../models/chat");
+const { stringify } = require("querystring");
 
 /* GET users listing. */
 router.get("/", async (req, res) => {
@@ -28,27 +30,27 @@ router.get("/:emailId", async (req, res) => {
 /*POST Create users */
 router.post("/", async (req, res) => {
   const userId = uuid.v1();
+  var password = req.body.password;
+  const buffer = uuid.v1();
+  const salt = buffer.substring(0, 10);
+  //console.log("\n salt", salt);
+  const hash = crypto.createHash("sha256");
+  //console.log("password with salt", password.concat(salt));
+  hash.update(password.concat(salt));
+  const hashedData = hash.digest("hex").toString();
+  //console.log("\n hashedData", hashedData);
+  password = hashedData.concat(salt);
+  //console.log("\n password", password);
   const users = await User({
     userId: userId,
     emailId: req.body.emailId,
-    password: req.body.password,
+    password: password,
   });
   const chats = await Chat({
     userId: userId,
     emailId: req.body.emailId,
     friends: [],
-    recieved: [
-      // {
-      //   friends: "prajesh@gmail.com",
-      //   chat: "hello",
-      //   time: "10",
-      // },
-      // {
-      //   friends: "prajesh@gmail.com",
-      //   chat: "hello!",
-      //   time: "10",
-      // },
-    ],
+    recieved: [],
     chat: [],
   });
   try {
@@ -58,8 +60,7 @@ router.post("/", async (req, res) => {
     res.status(400).json({ error: "Email must be unique" });
   }
   try {
-    const newChat = await chats.save();
-    //res.status(201).json(newChat);
+    await chats.save();
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -74,8 +75,19 @@ router.post("/login", async (req, res) => {
     if (!Users) {
       return res.status(404).json({ error: "User not found" });
     }
-
-    if (Users.password !== password) {
+    var uP = Users.password.toString();
+    //console.log("uP", Users.password);
+    var salt = uP.slice(-10);
+    //console.log("\n salt", salt);
+    const saltedPassword = password.concat(salt);
+    //console.log("\n saltedPassword", saltedPassword);
+    const hash = crypto.createHash("sha256");
+    hash.update(saltedPassword);
+    const hashedData = hash.digest("hex");
+    //console.log("\n hashedData", hashedData);
+    var hashedPasswordWithSalt = hashedData.concat(salt);
+    //console.log("\n hashedPasswordWithSalt", hashedPasswordWithSalt);
+    if (Users.password !== hashedPasswordWithSalt) {
       return res.status(401).json({ error: "Incorrect password" });
     }
 
@@ -87,6 +99,7 @@ router.post("/login", async (req, res) => {
       },
     });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Server error" });
   }
 });
--- server.js
+++ server.js
@@ -1,7 +1,41 @@
+// require("dotenv").config();
+// const express = require("express");
+// const uuid = require("uuid");
+// const mongoose = require("mongoose");
+// const WebSocket = require("ws");
+// var usersRouter = require("./routes/users_routes");
+// var chatRouter = require("./routes/chat_routes");
+
+// const port = 6000;
+// var app = express();
+
+// app.use(express.json());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // You can specify the allowed origins here
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+// app.use("/user", usersRouter);
+// app.use("/chat", chatRouter);
+
+// mongoose.set("strictQuery", false);
+// mongoose
+//   .connect(process.env.DB)
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`Example app listening on port ${port}`);
+//     });
+//     console.log("DB CONNECTED");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 require("dotenv").config();
 const express = require("express");
 const uuid = require("uuid");
 const mongoose = require("mongoose");
+const WebSocket = require("ws");
 var usersRouter = require("./routes/users_routes");
 var chatRouter = require("./routes/chat_routes");
 
@@ -10,7 +44,7 @@ var app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // You can specify the allowed origins here
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
@@ -22,11 +56,38 @@ mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.DB)
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
     console.log("DB CONNECTED");
   })
   .catch((err) => {
     console.log(err);
   });
+
+// Create an HTTP server using Express
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Create a WebSocket server on the same HTTP server
+const wss = new WebSocket.Server({ server });
+
+// WebSocket connection handling
+wss.on("connection", (ws) => {
+  console.log("WebSocket client connected");
+
+  // Handle WebSocket messages
+  ws.on("message", (message) => {
+    console.log(`Received message from WebSocket client: ${message}`);
+
+    // Example: Broadcast the received message to all connected WebSocket clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Handle WebSocket client disconnection
+  ws.on("close", () => {
+    console.log("WebSocket client disconnected");
+  });
+});
